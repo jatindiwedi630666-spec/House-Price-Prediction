8424311b23c09f35bee8a4376da6f5835698d97f
jatindiwedi630666-spec <jatindiwedi630666@gmail.com>
2025-12-29 10:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ y = df.iloc[:,-1]
 
 final_X = X
 scaler = StandardScaler()
-scated_Xfrom  = scaler.fit_transform(final_X)
+scaled_X = scaler.fit_transform(final_X)
 
 st.sidebar.title('Select House features: ')
-st.sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP1cvL7k_bZLr062cBg83QzkJGRiIXhqoOew&s')
+st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
 all_value = []
 for i in final_X:
-  min_value = final_X [i].min()
+  min_value = final_X[i].min()
   max_value = final_X[i].max()
   result = st.sidebar.slider(f'Select {i} value',min_value,max_value)
   all_value.append(result)
@@ -35,21 +35,14 @@ def ml_model(X,y):
   model.fit(X,y)
   return model
 
-model = ml_model(scated_X,y)
+model = ml_model(scaled_X,y)
 house_price = model.predict(user_X)[0]
 
 final_price = round(house_price * 100000,2)
 
-with st.spinner('prediting House Price'):
+with st.spinner('Predicting House Price'):
   import time
   time.sleep(2)
 
-st.success(f'Estimated House Price is:${final_price}')
-st.markdown('''**Design and Developed by:Jatin**''')
-
-
-
-
-
-
-
+st.success(f'Estimated House Price is: $ {final_price}')
+st.markdown('''**Design and Developed by: Ankit Mishra**''')
